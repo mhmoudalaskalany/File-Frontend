e3552bdc499439190fcf9140d7d879a53f5dd393
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2023-11-26 10:13:06 +0400

    remove unused components

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,32 +4,12 @@ import { CommonModule } from '@angular/common';
 // -------- Modules --------
 // NgSelect Module
 import { NgSelectModule } from '@ng-select/ng-select';
-// TableModule
-import { TableModule } from 'primeng/table';
 
-// Pipes
-import { ValidationHandlerPipe } from './pipes/validation-handler.pipe';
-
-// Components
-import { InputTextComponent } from './components/input-text/input-text.component';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { BaseSharedModule } from './sub-modules/base-shared/base-shared.module';
 
-const COMPONENTS = [InputTextComponent, DropdownComponent, CheckboxComponent];
-
 @NgModule({
-  declarations: [
-    // Components
-    ...COMPONENTS,
-
-    // Pipes
-    ValidationHandlerPipe
-  ],
-  imports: [CommonModule, BaseSharedModule, NgSelectModule, TableModule],
-  exports: [
-    // Components
-    ...COMPONENTS
-  ]
+  declarations: [],
+  imports: [CommonModule, BaseSharedModule, NgSelectModule],
+  exports: [BaseSharedModule]
 })
 export class SharedModule {}
